37a05c96b0df34ed99b7258c8bf8accd562c14dd
Cecilia Andrea Pesce <magicalkind@gmail.com>
2022-07-15 09:35:25 -0300

    first commit

--- Birthday/ViewController.swift
+++ Birthday/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var erickImageView: UIImageView!
+    @IBOutlet weak var happyBirthdayButton: UIButton!
+    @IBOutlet weak var stitchCakeImageView: UIImageView!
+    @IBOutlet weak var confettiAnimationView: AnimationView!
+    
+    lazy var confetti = Animation.named("confetti", bundle: .main)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        erickImageView.isHidden = true
+        stitchCakeImageView.isHidden = true
     }
-
-
+    
+    @IBAction func didTapHappyBirthdayButton(_ sender: Any) {
+        happyBirthdayButton.isHidden = true
+        erickImageView.isHidden = false
+        stitchCakeImageView.isHidden = false
+        throwConfetti()
+    }
+    
+    func throwConfetti() {
+        confettiAnimationView.animation = confetti
+        confettiAnimationView.loopMode = .playOnce
+        confettiAnimationView.play()
+    }
+    
 }
 
